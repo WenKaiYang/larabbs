5ca2529b4476acecc6f2cc55585b4de4407d8bcd
WenKaiYang <313097973@qq.com>
2021-08-06 16:26:43 +0800

    话题列表页面

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,5 +37,6 @@ class AppServiceProvider extends ServiceProvider
             return 'App\Policies\\' . class_basename($modelClass) . 'Policy';
         });
 
+        \Illuminate\Pagination\Paginator::useBootstrap();
     }
 }
