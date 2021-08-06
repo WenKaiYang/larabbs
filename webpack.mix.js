1ab95c8963efd09908cf39f3adcf59f4a3381a75
WenKaiYang <313097973@qq.com>
2021-08-06 17:18:48 +0800

    WYSIWYG 编辑器

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
-  .sourceMaps()
-  .version();
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps()
+    .version()
+    .copyDirectory('resources/editor/js', 'public/js')
+    .copyDirectory('resources/editor/css', 'public/css');
