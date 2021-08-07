aa2f385e92c04816a061ab4cff7ad1a209b1f4e3
WenKaiYang <313097973@qq.com>
2021-08-07 21:19:12 +0800

    集成 administrator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -43,7 +43,8 @@
                     </li>
 
                     <li class="nav-item notification-badge">
-                        <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white" href="{{ route('notifications.index') }}">
+                        <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white"
+                           href="{{ route('notifications.index') }}">
                             {{ Auth::user()->notification_count }}
                         </a>
                     </li>
@@ -57,6 +58,13 @@
                             {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            @can('manage_contents')
+                                <a class="dropdown-item" href="{{ url(config('administrator.uri')) }}">
+                                    <i class="fas fa-tachometer-alt mr-2"></i>
+                                    管理后台
+                                </a>
+                                <div class="dropdown-divider"></div>
+                            @endcan
                             <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
                                 <i class="far fa-user mr-2"></i>
                                 个人中心
