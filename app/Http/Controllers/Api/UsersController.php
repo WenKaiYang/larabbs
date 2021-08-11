ba7b550a59ac941ae14734f825fc1ae5d0f4a4e4
WenKaiYang <313097973@qq.com>
2021-08-11 12:07:51 +0800

    用户信息

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -36,4 +36,14 @@ class UsersController extends Controller
 
         return new UserResource($user);
     }
+
+    public function show(User $user, Request $request)
+    {
+        return new UserResource($user);
+    }
+
+    public function me(Request $request)
+    {
+        return (new UserResource($request->user()))->showSensitiveFields();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -56,8 +56,6 @@ Route::prefix('v1')
             });
 
 
-
-
         // 访问频率限制，次数/分钟
         Route::middleware('throttle:' . config('api.rate_limits.access'))
             ->group(function () {
@@ -67,6 +65,17 @@ Route::prefix('v1')
                     return 'this is version v1';
                 })->name('version');
 
+                // 游客可以访问的接口
+                // 某个用户的详情
+                Route::get('users/{user}', 'UsersController@show')
+                    ->name('users.show');
+
+                // 登录后可以访问的接口
+                Route::middleware('auth:api')->group(function () {
+                    // 当前登录用户信息
+                    Route::get('user', 'UsersController@me')
+                        ->name('user.show');
+                });
 
             });
 
